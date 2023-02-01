bb5e721bb9a4ea1d8b2c145da3ecb84eaebc203e
Alexandre Cardoso <xandecar@hotmail.com>
2023-02-01 13:41:19 -0300

    feat: use image again

--- public/index.php
+++ public/index.php
@@ -3,14 +3,30 @@
 
 <head>
   <script src="https://cdn.tiny.cloud/1/q32girmkj106tn6grxvn99jm1z4xqsdla3dnfweexj68mf8p/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+  <style>
+    .image {
+      cursor: pointer;
+    }
+  </style>
 </head>
 
 <body>
-  <textarea>
+  <?php
+  $images = scandir('assets/images');
+  foreach ($images as $image) :
+    if ($image !== '.' && $image !== '..') :
+  ?>
+      <img src="assets/images/<?php echo $image; ?>" alt="" width="200" height="100" class="image">
+  <?php
+    endif;
+  endforeach;
+  ?>
+  <hr>
+  <textarea id="image">
   </textarea>
   <script>
     tinymce.init({
-      selector: 'textarea',
+      selector: '#image',
       height: 1000,
       plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
       toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
@@ -60,6 +76,17 @@
         xhr.send(formData);
       })
     });
+
+    // colocar imagem no tinymce
+    const images = document.querySelectorAll('.image');
+    images.forEach(image => {
+      image.addEventListener('click', event => {
+        // tinymce.activeEditor.execCommand('mceInsertContent', false, image.outerHTML);
+        tinymce.get('image').execCommand('mceInsertContent', false, image.outerHTML);
+        // tinymce.activeEditor.setContent(image.outerHTML);
+        console.log(event);
+      })
+    })
   </script>
 </body>
 
